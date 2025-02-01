acf0f62aa3fae9e0a2640a217275247b4b8bef48
Saba karbakhsh <skarbakhshravari@my.bcit.ca>
2025-02-01 11:58:09 -0800

    .

--- COMP4537/labs/3/server.js
+++ COMP4537/labs/3/server.js
@@ -1,11 +1,12 @@
-const { FILE } = require('dns');
 let http = require('http');
 let url = require('url');
 let messages = require('./lang/en/en.js');
+const fs = require('fs');
 
 http.createServer(function (req, res) {
     let q = url.parse(req.url, true);
     let path = q.pathname;
+    console.log(path);
     if (path === '/COMP4537/labs/3/getDate'){
     res.writeHead(200, {'Content-Type': 'text/html'});
     let dateModule = require('./modules/utils.js');
@@ -13,18 +14,20 @@ http.createServer(function (req, res) {
     let name = q.query.name;
     let result = `<p style='color:blue'>`+ messages.userMessages.greetingString+ date + `</p>`;
     res.write(result.replace('%1', name));
+    res.end();
     }
     else if(path === '/COMP4537/labs/3/writeFile'){
     res.writeHead(200, {'Content-Type': 'text/plain'});
     let text = q.query.text;
     res.write(text);
     const fs = require('fs');
-    fs.appendFile('file.txt', text, function (err) {
+    fs.appendFile('file.txt', text + '\n', function (err) {
         if (err) {
             console.log('Error opening file');
             return;
         }
     });
+    res.end();
 }
 else if(path === '/COMP4537/labs/3/readFile'){
     res.writeHead(200, {'Content-Type': 'text/plain'});
