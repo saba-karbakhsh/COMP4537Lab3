bed18cfe927a92edafeaf568f92d98cb7aa214cf
Saba karbakhsh <skarbakhshravari@my.bcit.ca>
2025-02-01 15:23:19 -0800

    z

--- COMP4537/labs/3/server.js
+++ COMP4537/labs/3/server.js
@@ -9,42 +9,43 @@ http.createServer(function (req, res) {
     let path = q.pathname;
     res.writeHead(200, {'Content-Type': 'text/palin'});
     res.write(path);
-    if (path === '/COMP4537/labs/3/getDate'){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    let dateModule = require('./modules/utils.js');
-    let date = dateModule.getDate();
-    let name = q.query.name;
-    let result = `<p style='color:blue'>`+ messages.userMessages.greetingString+ date + `</p>`;
-    res.write(result.replace('%1', name));
     res.end();
-    }
-    else if(path === '/COMP4537/labs/3/writeFile'){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    let text = q.query.text;
-    res.write(text);
-    const fs = require('fs');
-    fs.appendFile('file.txt', text + '\n', function (err) {
-        if (err) {
-            console.log('Error opening file');
-            return;
-        }
-    });
-    res.end();
-}
-else if(path === '/COMP4537/labs/3/readFile/file.txt'){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    const fs = require('fs');
-    fs.readFile("file.txt", 'utf8', function(err, data) {
-        if (err) {
-            console.log('Error opening file');
-            return;
-        }
-        res.write(data);
-        res.end();
-    });
-}
-else{
-    res.writeHead(404, {'Content-Type': 'text/html'});
-    res.write('40444 Not Found');
-}
+//     if (path === '/COMP4537/labs/3/getDate'){
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     let dateModule = require('./modules/utils.js');
+//     let date = dateModule.getDate();
+//     let name = q.query.name;
+//     let result = `<p style='color:blue'>`+ messages.userMessages.greetingString+ date + `</p>`;
+//     res.write(result.replace('%1', name));
+//     res.end();
+//     }
+//     else if(path === '/COMP4537/labs/3/writeFile'){
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     let text = q.query.text;
+//     res.write(text);
+//     const fs = require('fs');
+//     fs.appendFile('file.txt', text + '\n', function (err) {
+//         if (err) {
+//             console.log('Error opening file');
+//             return;
+//         }
+//     });
+//     res.end();
+// }
+// else if(path === '/COMP4537/labs/3/readFile/file.txt'){
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     const fs = require('fs');
+//     fs.readFile("file.txt", 'utf8', function(err, data) {
+//         if (err) {
+//             console.log('Error opening file');
+//             return;
+//         }
+//         res.write(data);
+//         res.end();
+//     });
+// }
+// else{
+//     res.writeHead(404, {'Content-Type': 'text/html'});
+//     res.write('40444 Not Found');
+// }
 }).listen(process.env.PORT || 8888);
